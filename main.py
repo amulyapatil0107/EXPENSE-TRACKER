9d8c6d1131a8c61d1f5c326f5e3de1a03b006b30
amulyapatil0107 <amulyapatil0107@gmail.com>
2026-03-29 01:30:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template, request, redirect
-import os
-import json
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    # This tells Flask to look inside your 'templates' folder for the file
-    return render_template('index.html') 
-
-# Keep your delete and add logic here too!
+from src.expense_tracker.cli import main
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
+    main()
